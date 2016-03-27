14c56a9d69bb53337a9543433f1ce1ee16208c06
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2016-03-27 19:41:20 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ for i in range(N):
     r = list(input().split())
     if len(r)==2:
         G.add_edge(r[0],r[1])
-    elif len(r)==1:
+    else:
         G.add_node(r[0])
 nx.draw(G)
 plt.savefig("simple_path.png")
