b74793e06e31e884f629aa21d427f7b39fca6162
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2016-03-27 19:38:43 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,9 +4,9 @@ G = nx.Graph()
 N = int(input('Кол-во рёбер'))
 for i in range(N):
     r = list(input().split())
-    if len(r) == 2:
-        G.add_edge(r[0], r[1])
-    elif len(r) == 1:
+    if len(r)==2:
+        G.add_edge(r[0],r[1])
+    elif len(r)==1:
         G.add_node(r[0])
 nx.draw(G)
 plt.savefig("simple_path.png")
