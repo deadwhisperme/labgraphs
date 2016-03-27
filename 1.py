30b9f546c93dc5999b65317ec277f25a8b5a1a4a
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2016-03-27 19:34:21 +0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+G = nx.Graph()
+N = int(input('Кол-во рёбер'))
+for i in range(N):
+    r = list(input().split())
+    if len(r) == 2:
+        G.add_edge(r[0], r[1])
+    elif len(r) == 1:
+        G.add_node(r[0])
+nx.draw(G)
+plt.savefig("simple_path.png")
+plt.show()
