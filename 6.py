f9edecc7aa8c12c645070a2882a1b59ac60a0f96
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2016-04-03 19:36:07 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,36 +1,45 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 def graph():
-    N = int(input('Кол-во вершин'))
-    weight = [[float('+inf')]*N for j in range(N)]
-    for i in range(N):
-        weight[i][i] = 0
-    M = int(input('Кол-во ребер'))
-    for i in range(M):
-        a, b, w = input().split()
-        a, b, w = int(a), int(b), float(w)
-        weight[a][b] = w
-        weight[b][a] = w
-    return weight
+    inp3 = open("./inp3.txt","r")
+    ls = inp3.readlines()
+    G = {}
+    for i in range(len(ls)):
+        ls[i] = ls[i].rstrip()
+        a, b, weight = ls[i].split()
+        weight = float(weight)
+        if a not in G:
+            G[a] = {b:weight}
+        else:
+            G[a][b] = weight
+        if b not in G:
+            G[b] = {a:weight}
+        else:
+            G[b][a] = weight
+    return G
 
-def printmatrix(A):
-    for i in range(len(A)):
-        print(*A[i], sep='\t')
-    print()
-
-from copy import deepcopy
-
-def floyd(weight):
-    N = len(weight)
-    F = deepcopy(weight)
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                F[i][j] = min(F[i][j], F[i][k] + F[k][j])
-    return F
-
-
+def dejkstra(G, start):
+    shortest_path = {vertex:float('+inf') for vertex in G}
+    shortest_path[start] = 0
+    queue = [start]
+    while queue:
+        current = queue.pop(0)
+        for neighbour in G[current]:
+            offering_shortest_path = shortest_path[current] + G[current][neighbour]
+            if offering_shortest_path < shortest_path[neighbour]:
+                shortest_path[neighbour] = offering_shortest_path
+                queue.append(neighbour)
+    return shortest_path
+def shortest(G, start, finish):
+    d = dejkstra(G, start)
+    r = finish
+    p=[]
+    while r:
+        p.append(r)
+        r = d[r][1]
+    return p[::-1]
 G = graph()
-printmatrix(G)
-F = floyd(G)
-printmatrix(F)
+begin = input('Начальная точка ')
+finish = input('Конечная точка ')
+print(shortest(G,begin,finish))
+
