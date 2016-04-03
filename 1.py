08f79267e0fd3d221cd0a2e3b9a62b01fdabd9dd
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2016-04-03 19:17:31 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 G = nx.Graph()
-N = int(input('Кол-во рёбер'))
-for i in range(N):
-    r = list(input().split())
+inp = open("./inp.txt","r")
+ls = inp.readlines()
+for i in range(len(ls)):
+    ls[i] = ls[i].rstrip()
+    r = list(ls[i].split())
     if len(r)==2:
         G.add_edge(r[0],r[1])
     else:
